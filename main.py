971f1dda3ddc60bad4b19d5bdd694b27fb93dc96
TalDoHiki <122174430+TalDoHiki@users.noreply.github.com>
2023-01-07 15:35:18 -0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,92 @@
+#print("Te amo")
+#print("Akemin!")
+
+#fn = "Felpe"
+#ln = "Hikimin"
+#fl = fn+" "+ln
+#print("Hello "+fl)
+
+#n = input("What's your name?: ")
+#age = int(input("How old are you?: "))
+#print("Hello "+n)
+#print("You're "+str(age)+" years old, cool!")
+#if(int(age)<18):{
+#print("But that's a shame, you're a minnor, "+n)
+#}
+
+import math
+import time
+
+#n = input("What's the square of 4?: ")
+#if(int(n)==2):
+#    print("Good, you're right!")
+#else:
+#    print("Damn, you miss :(")
+
+#website = "http://google.com"
+#slice = slice(7,-4)
+# print(website[slice])
+
+#age = int(input("How much older are you?: "))
+#if age>=100:
+#   age = (age/100)
+#   print("You're ",str(age)," century(ies) old!")
+#elif age>=18:
+#    print("You're an adult!")
+#else:
+#    print("You're a child!")
+
+#name = None
+#while not name:
+#    name = input("Enter your name: ")
+#print ("Hello "+name)
+#for i in range(10):
+#    print(i+1)
+#for i in range(50,100+1,2):
+#    print(i)
+#for i in "Felpe":
+#    print(i)
+
+
+
+#for seconds in range(0,60,1):
+#    time.sleep(0)
+#    print(hours,":",minutes,":",seconds)
+#if seconds == 60:
+#    minutes + 1
+#if minutes == 60:
+#    minutes = 0
+#    hours = 1
+#if hours == 1:
+#    seconds = minutes = hours = 0
+
+#rows = int(input("How many rows?: "))
+#columns = int(input("How mane columns?: "))
+#symbol = input("Enter a symbol to use: ")
+#for i in range(rows):
+#    for j in range(columns):
+#        print(symbol, end="")
+#    print()
+
+#while True:
+#    name = input("Enter your name: ")
+#    if name !="":
+#        break
+
+#ph = "423-4126-7890"
+#for i in ph:
+#    if i == "-":
+#        continue
+#    print(i, end="")
+
+#food = ["pizza","hamburger","hotdog","spaghetti"]
+#for x in food:
+#    print(x, end=", ")
+
+drinks = ["coffe","soda","tea"]
+dinner = ["rice", "pizza"]
+dessert = ["cake","ice cream"]
+
+food = [drinks,dinner,dessert]
+
+print(food[2])
